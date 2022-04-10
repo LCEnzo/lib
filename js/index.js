f1f84f8b7456bd01dbdbb4c26f32bde528c5f82e
LCEnzo <lcenzo@protonmail.ch>
2022-04-10 20:20:03 +0200

    .

--- js/index.js
+++ js/index.js
@@ -3,23 +3,101 @@
 let books = [];
 const testing = true;
 
-function Book(title, author, pages, read) {
+const display = document.querySelector("#book-display");
+
+function BIDmanager(startingSize = 100) {
+    this.prototype.size = Number(startingSize) ?? 100;
+    this.prototype.bids = []
+
+    for(let i = 0; i < this.prototype.size; i++) {
+        bids.push(i);
+    }
+
+    this.getNewId = () => {
+        if(bids.length < 3) {
+            const oldSize = this.prototype.size;
+            this.prototype.size = 2 * this.prototype.size;
+
+            for(let i = oldSize; i < this.prototype.size; i++) {
+                bids.push(i);
+            }
+        }
+
+        return bids.shift();
+    }
+
+    this.retireOldId = (oldId) => {
+        let num = Number(oldId) ?? -1;
+
+        if(num > -1) {
+            bids.push(num);
+        }
+    }
+}
+
+const bidManager = new BIDmanager();
+
+function Book(title, author, pages, readPages = 0, read = false) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.pages = Number(pages);
+    this.readPages = Number(readPages);
+    this.read = Boolean(read) || (pages == readPages);
+
+    Object.defineProperty(this, "title", {
+        writable: false,
+    });
+    
+    Object.defineProperty(this, "author", {
+        writable: false,
+    });
+
+    Object.defineProperty(this, "pages", {
+        writable: false,
+    });
+
+    Object.defineProperty(this, "bid", {
+        value: getNewId(),
+        enumerable: true,
+        writable: false,
+        configurable: false 
+    });
 
     this.info = () => {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`;
     }
 }
 
-function addBookToLib(book) {
+function updateDisplay() {
+    // TODO, adds new books to display, removes old ones
+    let cards = document.querySelectorAll(".card");
+
+    display.childNodes.
+}
+
+function addBook(book, update = true) {
     books.push(book);
+
+    if(update) {
+        updateDisplay();
+    }
+}
+
+function removeBook(book, update = true) {
+    let index = books.indexOf(book);
+
+    if(index !== -1) {
+        books.splice(index, 1);
+        bidManager.retireOldId(book.bid);
+
+        if(update) {
+            updateDisplay();
+        }
+    }
 }
 
 if(testing) {
-    addBookToLib(new Book("Hail Mary", "Andy Weir", "620", "620"));
-    addBookToLib(new Book("Working Backwards", "Ex amazon people", "411", "328"));
-    addBookToLib(new Book("From Third World to First", "Lee Kuan Yew", "606", "0"));
+    addBook(new Book("Hail Mary", "Andy Weir", 620, 620));
+    addBook(new Book("Working Backwards", "Ex amazon people", "411", "328"));
+    addBook(new Book("From Third World to First", "Lee Kuan Yew", 606, 0));
 }
